93ab9762f999484a8175e0c6fa1e1297ace3acbd
Maciej Nowakowski <maciej.nowakowski@allegrogroup.com>
2017-03-04 17:18:43 +0100

    Improving script

--- install_on_osx_qt5.sh
+++ install_on_osx_qt5.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 BUILD_DIRECTORY="./build"
 
+# Requiremnts
+
+command -v bre >/dev/null 2>&1 || { echo >&2 "I require Homebrew but it's not installed. 
+Download from: https://brew.sh/ 
+Aborting."; exit 1; }
+
+
+
 cd ~/
 git clone --recursive git@github.com:TheMickeyMike/hyperion.git ~/hyperion_osx
 cd ~/hyperion_osx
@@ -10,11 +18,11 @@ if [ -d "$BUILD_DIRECTORY" ]; then
 fi
 mkdir $BUILD_DIRECTORY
 cd $BUILD_DIRECTORY
-cmake -DENABLE_DISPMANX=OFF \
+cmake -Wno-dev \
+-DENABLE_DISPMANX=OFF \
 -DENABLE_SPIDEV=OFF \
 -DENABLE_V4L2=OFF \
 -DENABLE_OSX=ON \
--DENABLE_PROTOBUF=OFF \
 -DENABLE_QT5=ON ..
 
 make
@@ -23,3 +31,9 @@ mkdir -p ~/hyperion/bin
 cp bin/hyperiond ~/hyperion/bin
 cp -r ../effects ~/hyperion/
 cd ~/hyperion
+
+
+
+# brew install qt
+# echo 'export PATH="/usr/local/opt/qt5/bin:$PATH"' >> ~/.bash_profile
+source ~/.bash_profile
---
+++ test.sh
@@ -0,0 +1,3 @@
+command -v brew >/dev/null 2>&1 || { echo >&2 "I require Homebrew but it's not installed. 
+Download from: https://brew.sh/ 
+Aborting."; exit 1; }
