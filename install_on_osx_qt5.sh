7199634595c6f94edd5c0d20f55f46dd19edff7e
Maciej Nowakowski <macnow@st.amu.edu.pl>
2017-03-05 00:33:24 +0100

    Small refactoring & improvements

--- install_on_osx_qt5.sh
+++ install_on_osx_qt5.sh
@@ -15,25 +15,30 @@ HYPERION_DAEMON=hyperiond
 
 
 # Requiremnts Check
-command -v brew >/dev/null 2>&1 || { printf "${RED}[FAIL]${NC}\tHomebrew\nI require Homebrew but it's not installed.\nDownload from: https://brew.sh/\nAborting.\n";
+command -v brew >/dev/null 2>&1 || { echo -e "${RED}[FAIL]${NC}\tHomebrew\nI require Homebrew but it's not installed.\nDownload from: https://brew.sh/\nAborting.";
  exit 1; }
-printf "${GREEN}[OK]${NC}\tHomebrew\n";
+echo -e "${GREEN}[OK]${NC}\tHomebrew";
 
 
-if [[ $(brew list) != *qt5* ]] 
-then
-	printf "${RED}[FAIL]${NC}\tQt5\nI require Qt5 but it's not installed.\nTo install: brew install qt5\nAborting.\n";
-	exit 1;
-else
-	printf "${GREEN}[OK]${NC}\tQT5\n";
+if [[ $(brew list) != *qt5* ]]; then
+	echo -e "[INFO]\tI require Qt5 but it's not installed.\nInstalling Qt5...";
+	brew install qt5
+	export PATH=$PATH:/usr/local/opt/qt5/bin
 fi
+echo -e "${GREEN}[OK]${NC}\tQt5";
+
+if [[ $(brew list) != *libusb* ]]; then
+	echo -e "[INFO]\tI require libusb but it's not installed.\nInstalling libusb...";
+	brew install libusb
+fi
+echo -e "${GREEN}[OK]${NC}\tlibusb";
 
 if [[ $(qmake -v) =~ "5" ]]
 then
-	printf "${GREEN}[OK]${NC}\tQt5 PATH\n";
+	echo -e "${GREEN}[OK]${NC}\tQt5 PATH";
 else
-	printf "${RED}[FAIL]${NC}\tQt5 PATH is not set\n";
-	printf 'Try appending ~/.bash_profile with: /usr/local/opt/qt5/bin:$PATH\n'
+	echo -e "${RED}[FAIL]${NC}\tQt5 PATH is not set";
+	echo -e "Try appending ~/.bash_profile with: /usr/local/opt/qt5/bin:\$PATH"
 	exit 1;
 fi
 # End Requiremnts Check
@@ -49,34 +54,34 @@ then
     	rm -rf $REPO_DIRECTORY
 	fi
 else
-	printf "[INFO]\tCloning hyperion repo...\n"
+	echo -e "[INFO]\tCloning hyperion repo..."
 	git clone --recursive https://github.com/TheMickeyMike/hyperion.git $REPO_DIRECTORY
 fi
 
 cd $REPO_DIRECTORY || exit 1;
-printf "${GREEN}[OK]${NC}\tHyperion repo\n";
+echo -e "${GREEN}[OK]${NC}\tHyperion repo";
 mkdir $BUILD_DIRECTORY
 cd $BUILD_DIRECTORY || exit 1;
 
-printf "[INFO]\tExecuting: cmake\n"
+echo -e "[INFO]\tExecuting: cmake"
 cmake -Wno-dev \
 -DENABLE_DISPMANX=OFF \
 -DENABLE_SPIDEV=OFF \
 -DENABLE_V4L2=OFF \
 -DENABLE_OSX=ON \
 -DENABLE_QT5=ON ..
-printf "${GREEN}[OK]${NC}\tcmake\n";
+echo -e "${GREEN}[OK]${NC}\tcmake";
 
-printf "[INFO]\tExecuting: make\n"
-make -j $(sysctl -n hw.ncpu)
-printf "${GREEN}[OK]${NC}\tmake\n";
+echo -e "[INFO]\tExecuting: make"
+make -j "$(sysctl -n hw.ncpu)"
+echo -e "${GREEN}[OK]${NC}\tmake";
 
-printf "[INFO]\tStarting installation...\n"
+echo -e "[INFO]\tStarting installation..."
 if [ -d "$HYPERION_INSTALLATION_MAIN_DIRECTORY" ]; then
   rm -rf $HYPERION_INSTALLATION_MAIN_DIRECTORY
 fi
 sudo mkdir $HYPERION_INSTALLATION_MAIN_DIRECTORY
-sudo chown $(whoami):admin $HYPERION_INSTALLATION_MAIN_DIRECTORY
+sudo chown "$(whoami)":admin $HYPERION_INSTALLATION_MAIN_DIRECTORY
 
 if [ -d "$HYPERION_INSTALLATION_BIN_DIRECTORY" ]; then
   rm -rf $HYPERION_INSTALLATION_BIN_DIRECTORY
@@ -96,16 +101,15 @@ mkdir $HYPERION_INSTALLATION_CONFIG_DIRECTORY
 cp ./bin/$HYPERION_DAEMON $HYPERION_INSTALLATION_BIN_DIRECTORY
 cp ./bin/hyperion-remote $HYPERION_INSTALLATION_BIN_DIRECTORY
 cp ./bin/hyperion-osx $HYPERION_INSTALLATION_BIN_DIRECTORY
-cd ../effects && cp * $HYPERION_INSTALLATION_EFFECTS_DIRECTORY
+cd ../effects && cp ./* $HYPERION_INSTALLATION_EFFECTS_DIRECTORY
 ln -s $HYPERION_INSTALLATION_BIN_DIRECTORY/$HYPERION_DAEMON $HYPERION_BIN_DIRECTORY/$HYPERION_DAEMON
-printf "${GREEN}[OK]${NC}\tInstallation\n";
+echo -e "${GREEN}[OK]${NC}\tInstallation";
 
-printf "[INFO]\tPerforming post installation checks...\n"
+echo -e "[INFO]\tPerforming post installation checks..."
 if ! type "$HYPERION_DAEMON" > /dev/null; then
-	printf "${RED}[FAIL]${NC}\tCan't launch hyperiond\n";
+	echo -e "${RED}[FAIL]${NC}\tCan't launch hyperiond";
 	exit 1;
 fi
-printf "${GREEN}[OK]${NC}\tInstallation\n";
-printf "Success! Now you can use hyperion with command: hyperiond\nExample: hyperiond config.json\n"
+echo -e "${GREEN}[OK]${NC}\tInstallation";
+echo -e "Success! Now you can use hyperion with command: hyperiond\nExample: hyperiond config.json"
 exit 0;
-
